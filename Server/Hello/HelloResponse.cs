d931ed3ce8127825262bb6d1a0dd516d12cbe4e2
Fablo Alencar <fablo.alencar@hotmail.com>
2019-11-08 16:20:18 +0100

    Marcus is putting pressure on me

---
+++ Server/Hello/HelloResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Server.Hello
+{
+    public class HelloResponse
+    {
+        public string Message { get; set; }
+
+        public HelloResponse(string message)
+        {
+            Message = message;
+        }
+    }
+}
